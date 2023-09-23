35713009b4c5f5fea643cfa2a7d0e508da04aa14
HagerRamadan65 <136929535+HagerRamadan65@users.noreply.github.com>
2023-09-23 06:52:18 +0300

    Update contact.js

--- contact/contact.js
+++ contact/contact.js
@@ -1,1 +1,26 @@
+var btnSubmit=document.querySelector('.btn-submit')
+var inputs=document.querySelectorAll('.input')
+var emptyInput=document.querySelector('.failed')
+var submitError=document.querySelector('.submit-error')
+var done=document.querySelector('.message-done')
+var form=document.querySelector('.form_row')
 
+
+
+
+function validationContact() {
+    btnSubmit.addEventListener('click',function () {
+     inputs.forEach(input=>{
+        if (input.value.length==0) {
+            input.parentElement.lastElementChild.innerHTML=`
+                <p>Required field</p>
+`
+submitError.innerHTML=`<p>Please, fill all the required fields</p>
+`
+            
+        }
+   
+     })
+     })
+}
+validationContact()
